3dcd1605aa762685b5ff27b8afecda5fe029c6c2
Lklak <95124815+Lklak@users.noreply.github.com>
2021-11-27 16:57:42 +0700

    Update and rename atlmake.php to autoloadmaker.php

--- autoloadmaker.php
+++ autoloadmaker.php
@@ -1,32 +1,4 @@
 <?php
-function getFileMimeType($file) {
-    if (function_exists('finfo_file')) {
-        $finfo = finfo_open(FILEINFO_MIME_TYPE);
-        $type = finfo_file($finfo, $file);
-        finfo_close($finfo);
-    } else {
-        require_once 'upgradephp/ext/mime.php';
-        $type = mime_content_type($file);
-    }
-
-    if (!$type || in_array($type, array('application/octet-stream', 'text/plain'))) {
-        $secondOpinion = exec('file -b --mime-type ' . escapeshellarg($file), $foo, $returnCode);
-        if ($returnCode === 0 && $secondOpinion) {
-            $type = $secondOpinion;
-        }
-    }
-
-    if (!$type || in_array($type, array('application/octet-stream', 'text/plain'))) {
-        require_once 'upgradephp/ext/mime.php';
-        $exifImageType = exif_imagetype($file);
-        if ($exifImageType !== false) {
-            $type = image_type_to_mime_type($exifImageType);
-        }
-    }
-
-    return $type;
-}
-
 function strpos_recursive($haystack, $needle, $offset = 0, &$results = array()) {               
     $offset = strpos($haystack, $needle, $offset);
     if($offset === false) {
@@ -98,26 +70,4 @@ foreach ($meyw as $datatoprint){
 }
 fwrite($myfile, "?>");
 fclose($myfile);
-echo getfex("ak/als/autoload.php") . ":autoload.php";
-/*
-if (empty($_GET['dir'])){
-    $dir = "./Symfony/";
-}else{
-    $dir = urldecode($_GET['dir']);
-}
-
-if (is_file($dir)){
-    header('Content-Type:' . mime_content_type($dir));
-    readfile($dir);
-}else{
-    $files = array_diff(scandir($dir), array('.', '..'));
-    foreach ($files as $fname){
-        if (is_file($dir . $fname)){
-            echo "<a href='?dir=" . urlencode($dir . $fname) . "'>$fname</a><br />";
-        }else{
-            echo "<a href='?dir=" . urlencode($dir . $fname . "/") . "'>$fname</a><br />";
-        }
-    }
-}
-*/
 ?>
